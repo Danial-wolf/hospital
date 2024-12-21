99594c590b5985fb4c141fab10bc850252f2d291
danial <danial.tasawar007@gmail.com>
2024-12-21 12:06:11 +0500

    change message for age

--- Doctor.cpp
+++ Doctor.cpp
@@ -4,9 +4,6 @@
 using namespace std;
 
 
-
-
-
 Doctor::Doctor(string name, int age, string gender, string specialization, int experiance): Person(name,age,gender),specialization(specialization),experiance(experiance) 
 {
     totalDoctor++;
@@ -14,7 +11,7 @@ Doctor::Doctor(string name, int age, string gender, string specialization, int e
 void Doctor::display()
 {
     cout<<"Doctor Name is "<< name <<endl;
-    cout<<"Age is "<< age <<endl;
+    cout<<"Doctor Age is "<< age <<endl;
     cout<<"Gender is "<< gender <<endl;
     cout<<"Specialization in "<< specialization <<endl;
     cout<<"Year of experiance "<< experiance <<endl;
